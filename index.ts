99d9bd39b8e49374722442d49fe0abdd67dbd03e
BDani1990 <51000350+BDani1990@users.noreply.github.com>
2022-09-23 10:40:56 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,22 +1,54 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 console.clear();
 
 const o1 = new Observable((observer) => {
   let counter = 0;
-  document.querySelector('#event-square').addEventListener('click', () => {
+
+  const eventClickListener = () => {
     counter++;
     console.log('Event emitig');
     observer.next(counter);
-  });
-
-  document.querySelector('#error').addEventListener('click', () => {
+  };
+  document
+    .querySelector('#event-square')
+    .addEventListener('click', eventClickListener);
+  /*------------------------------------------------------*/
+  const errorClickListener = () => {
     console.log('Error emitig');
     observer.error('hiba történt');
-  });
+  };
+  document
+    .querySelector('#error')
+    .addEventListener('click', errorClickListener);
+  /*------------------------------------------------------*/
+  const completeClickListener = () => {
+    console.log('Complete emitig');
+    observer.complete();
+  };
+  document
+    .querySelector('#complete')
+    .addEventListener('click', completeClickListener);
+  /*------------------------------------------------------*/
+  return () => {
+    document
+      .querySelector('#event-square')
+      .removeEventListener('click', eventClickListener);
+
+    document
+      .querySelector('#error')
+      .removeEventListener('click', errorClickListener);
+
+    document
+      .querySelector('#complete')
+      .removeEventListener('click', completeClickListener);
+  };
 });
 
-const subscription = o1.subscribe(
+const s = new Subject();
+o1.subscribe(s);
+
+const subscription = s.subscribe(
   (res) => {
     console.log('Observable emits', res);
     if (res === 5) {
@@ -25,5 +57,23 @@ const subscription = o1.subscribe(
   },
   (err) => {
     console.log('Observable error', err);
+  },
+  () => {
+    console.log('Observable complete');
+  }
+);
+
+const subscription2 = s.subscribe(
+  (res) => {
+    console.log('Observable emits', res);
+    if (res === 5) {
+      subscription.unsubscribe();
+    }
+  },
+  (err) => {
+    console.log('Observable error', err);
+  },
+  () => {
+    console.log('Observable complete');
   }
 );
